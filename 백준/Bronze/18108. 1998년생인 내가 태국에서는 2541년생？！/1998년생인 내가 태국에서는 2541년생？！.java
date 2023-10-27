8f51e0d8df7b4269a8b3fe812c79ffdc30509131
이지수 <efforterjisulee@gachon.ac.kr>
2023-10-27 23:06:23 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int tYear = scanner.nextInt();
+        int kYear = tYear - 543;
+        System.out.println(kYear);
+    }
+}
