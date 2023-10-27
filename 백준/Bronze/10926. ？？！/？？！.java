1c54bb2afcc7ac90b3563b54194abc008e0ba9b2
이지수 <efforterjisulee@gachon.ac.kr>
2023-10-27 22:57:40 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        String lastStr = "??!";
+
+        System.out.println(str + lastStr);
+    }
+}
